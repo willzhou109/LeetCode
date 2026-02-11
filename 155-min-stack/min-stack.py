74ee5f4709f609dff9d43d4215ddbb5db074bdba
William Zhou <willzhou109@gmail.com>
2026-02-10 21:59:51 -0500

    Time: 8 ms (65.13%) | Memory: 15.9 MB (34.50%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,36 @@
+class MinStack(object):
+
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        if len(self.minStack) == 0 or val < self.minStack[-1]:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(self.minStack[-1])
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minStack.pop()
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
